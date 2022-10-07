5ff5fac7261675fdf49cf869e0e670e5900e3fb7
10190172 <10190172@B206PC12>
2022-10-07 12:15:48 +0300

    dsf

---
+++ WpfApp1/AddEdit.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.DataContent;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+
+    
+    public partial class Page1 : Page
+    {
+        private Agent _currentAgent = new Agent();
+
+        //bool isValid(string )
+        //{
+        //    string pattern = "[.\\-_a-z0-9]+@([a-z0-9][\\-a-z0-9]+\\.)+[a-z]{2,6}";
+        //    Match isMatch = Regex.Match(, pattern, RegexOptions.IgnoreCase);
+        //    return isMatch.Success;
+        //}
+
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -17,14 +17,14 @@ using System.Windows.Media.Effects;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
 			AppContent.Model1 = new Entities();
 			Filtr.Items.Add("Все типы");
 			foreach (var i in AppContent.Model1.AgentType)
@@ -33,7 +33,7 @@ namespace WpfApp1
 			}
 			Sort.Items.Add("Без сортировки");
 			Sort.Items.Add("По названию a-я");
-			Sort.Items.Add("По названию я-а"); 
+			Sort.Items.Add("По названию я-а");
 			Sort.Items.Add("По возрастанию");
 			Sort.Items.Add("По убыванию");
 			Sort.SelectedIndex = 0;
@@ -44,14 +44,14 @@ namespace WpfApp1
 		}
 
 		public void UpdateAgent()
-        {
+		{
 			var CurrentAgent = Entities.GetContext().Agent.ToList();
 
-			if(Filtr.SelectedIndex > 0)
-            {
+			if (Filtr.SelectedIndex > 0)
+			{
 				var test = Filtr.SelectedItem.ToString();
 				CurrentAgent = CurrentAgent.Where(p => p.AgentType.Title == Filtr.SelectedItem.ToString()).ToList();
-            }
+			}
 
 			CurrentAgent = CurrentAgent.Where(p => p.Title.ToLower().Contains(Search.Text.ToLower())).ToList();
 
@@ -63,14 +63,31 @@ namespace WpfApp1
 			UpdateAgent();
 		}
 
-        private void Sort_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-			UpdateAgent();
-		}
+		private void Sort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var CurrentAgent = Entities.GetContext().Agent.ToList();
 
-        private void Search_SelectionChanged(object sender, RoutedEventArgs e)
-        {
+			switch (Sort.SelectedIndex)
+			{
+				case 1:
+					Agent.ItemsSource = CurrentAgent.OrderBy(p => p.Title).ToList();
+					break;
+				case 2:
+					Agent.ItemsSource = CurrentAgent.OrderByDescending(p => p.Title).ToList();
+					break;
+				case 3:
+					Agent.ItemsSource = CurrentAgent.OrderBy(p => p.Title).ToList();
+					break;
+				case 4:
+					Agent.ItemsSource = CurrentAgent.OrderByDescending(p => p.Title).ToList();
+					break;
+			}
+			UpdateAgent();
+
+		}
+		private void Search_SelectionChanged(object sender, RoutedEventArgs e)
+		{
 			UpdateAgent();
 		}
-    }
+	}
 }
